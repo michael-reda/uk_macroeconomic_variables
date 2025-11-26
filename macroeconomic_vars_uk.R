25d4216b394d6b1967442cde58ad49efb5fd9d9d
michael-reda <Michael.Reda@PRDDAPINFSR2442.DEFRA.ONMICROSOFT.COM>
2025-11-26 17:29:30 +0000

    Finished transforming dataframes. Half way through QAing them and checking for further necessary transformations. Added GDP deflator.

--- macroeconomic_vars_uk.R
+++ macroeconomic_vars_uk.R
@@ -30,6 +30,8 @@ boe_annual_df <- boe_exchg_interest_df %>%
   group_by(year)%>%
   summarise(across(2:5, mean, na.rm = TRUE))
 
+rm(boe_exchg_interest_df)
+
 # Institute for Fiscal Studies living standards, poverty and inequality data
 # https://ifs.org.uk/living-standards-poverty-and-inequality-uk
 # net [of taxes and inclusive of benefits] equivalised household income, before housing costs, inflation adjusted.
@@ -58,9 +60,16 @@ ifs_df <- dplyr::left_join(ifs_income_bhc_df, ifs_inequality_df, by = "Year")%>%
 ifs_df <- ifs_df %>% mutate(Year = stringr::str_sub(Year, 1, 4))%>%
   rename(year = Year,
          mean_household_income = `Mean income`,
-         median_household_income = `Median income`
-  )
+         median_household_income = `Median income`,
+         gini_coefficient = `Gini coefficient`
+  )%>%
+  mutate(year = as.numeric(year))
   
+rm(ifs_child_poverty_bhc_df,
+   ifs_income_bhc_df,
+   ifs_inequality_df,
+   ifs_poverty_bhc_df)
+
 # ONS public sector finance data
 # https://www.ons.gov.uk/economy/governmentpublicsectorandtaxes/publicsectorfinance/bulletins/publicsectorfinances/september2025
 # quarterly_public_sector_net_borrowing : J5II
@@ -82,7 +91,7 @@ names(public_sector_finances_df) <- c("year",
 
 
 public_sector_finances_df <- public_sector_finances_df %>%
-  slice(9:87)
+  slice(15:93)
 
 public_sector_finances_df[] <- lapply(public_sector_finances_df[], as.numeric)
 
@@ -99,7 +108,8 @@ gfcf_df <- readxl::read_xlsx("data/ons_grossfixedcapitalformationbysectorandasse
   mutate(gfcf = as.numeric(gfcf),
          date_qtrly = lubridate::yq(date_qtrly),
          year = lubridate::year(date_qtrly)
-  )%>% group_by(year)%>%
+  )%>% 
+  group_by(year)%>%
   summarise(gfcf = sum(gfcf))%>%
   ungroup()
   
@@ -131,6 +141,7 @@ capital_account_balance_df <- readr::read_csv(file = "data/ons_capital_account.c
          capital_account_balance_cp = as.numeric(capital_account_balance_cp)
   )
 
+# bookmark
 
 # ONS trade stats, 1997 - 2025, Chain Volume Measured, EU, non-EU, total, goods, services, imports, exports
 # https://www.ons.gov.uk/economy/nationalaccounts/balanceofpayments/datasets/tradeingoodsmretsallbopeu2013timeseriesspreadsheet
@@ -173,30 +184,86 @@ trade_balance_df <- readxl::read_xlsx("data/ons_uk_trade_stats.xlsx", sheet = "d
            "Trade in Services (TS): WW: Balance: BOP: CVM: SA"))%>%
   slice(59:86)
 
+trade_balance_variable_names <- c("year",
+                                  "exports_total", 
+                                  "imports_total", 
+                                  "balance_total", 
+                                  "goods_imports_total", 
+                                  "goods_exports_total",
+                                  "goods_balance_total",
+                                  "goods_balance_eu",
+                                  "goods_balance_non_eu",
+                                  "goods_exports_eu",
+                                  "goods_imports_eu",
+                                  "goods_exports_non_eu",
+                                  "goods_imports_non_eu",
+                                  "services_exports_total",
+                                  "services_imports_total",
+                                  "services_balance_total"
+)
+
+names(trade_balance_df) <- trade_balance_variable_names
+
+trade_balance_df[] <- lapply(trade_balance_df[], as.numeric)
 
 # ONS quarterly GDP in chained volume measures (2023 prices)
 # sheet= "2018 -" array= "BH165:B286"
 # https://www.ons.gov.uk/economy/grossdomesticproductgdp/datasets/realtimedatabaseforukgdpabmi
 gdp_df <- readxl::read_xlsx("data/ons_uk_quarterly_GDP_real.xlsx", sheet = "2018 - ", range = "A4:BH286")%>%
-  select(1, 60)
+  select(1, 60)%>%
+  mutate(year = substr(`Publication date and time period`, 4, 7))%>%
+  mutate(year = as.numeric(year))%>%
+  select(year, gdp = `Sep-25 [2023 prices]\r\nQNA`)%>%
+  group_by(year)%>%
+  summarise(gdp = sum(gdp)
+  )%>%
+  ungroup()%>%
+  filter(year != 2025)
+
 
 # ONS CPI
 # https://www.ons.gov.uk/economy/inflationandpriceindices/datasets/consumerpriceindices
-cpi_df <- readr::read_csv("data/ons_uk_cpi_89_25.csv")
+cpi_df <- readr::read_csv("data/ons_uk_cpi_89_25.csv")%>%
+  slice(8:43)%>%
+  mutate(year = as.numeric(Title),
+         cpi = as.numeric(`CPI ANNUAL RATE 00: ALL ITEMS 2015=100`)
+  )%>%
+  select(year, cpi)
+
+# GDP deflator
+#https://www.ons.gov.uk/economy/grossdomesticproductgdp/timeseries/ihys/qna
+gdp_deflator_df <- readr::read_csv("data/ons_gdp_deflator_yoy_growth.csv")%>%
+  slice(8:83)%>%
+  mutate(year = as.numeric(Title),
+         gdp_deflator_growth = as.numeric(`GDP Deflator: Year on Year growth: SA %`)
+  )%>%
+  select(year, gdp_deflator_growth)
+
 
 # ONS Unemployment rate (aged 16 and over, seasonally adjusted): %
 # https://www.ons.gov.uk/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/mgsx/lms
-unemployment_df <- readr::read_csv("data/ons_uk_unemployment_71_24.csv")
+unemployment_df <- readr::read_csv("data/ons_uk_unemployment_71_24.csv")%>%
+  slice(8:61)%>%
+  mutate(year = as.numeric(Title),
+         unemployment_rate = as.numeric(`Unemployment rate (aged 16 and over, seasonally adjusted): %`)
+  )%>%
+  select(year, unemployment_rate)
 
 # ONS population
 # https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/timeseries/ukpop/pop
-population_df <- readr::read_csv("data/ons_uk_population_71_24.csv")
+population_df <- readr::read_csv("data/ons_uk_population_71_24.csv")%>%
+  slice(8:61)%>%
+  mutate(year = as.numeric(Title),
+         population = as.numeric(`United Kingdom population mid-year estimate`)
+  )%>%
+  select(year, population)
 
 
 # trade intensity (% of GDP), World Bank
 # https://data.worldbank.org/indicator/NE.TRD.GNFS.ZS
 trade_intensity_df <- readr::read_csv("data/wb_trade_intensity.csv", skip = 3)%>%
   filter(`Country Code` == "GBR")
+
 
 # Research and development expenditure (% of GDP), World Bank
 # https://data.worldbank.org/indicator/GB.XPD.RSDV.GD.ZS
@@ -212,10 +279,43 @@ manuf_val_added_df <- readr::read_csv("data/wb_manuf_val_added.csv", skip = 3)%>
 urban_pop_pct_df <- readr::read_csv("data/wb_urban_pop_pct.csv", skip = 3)%>%
   filter(`Country Code` == "GBR")
 
+wb_df <- rbind(trade_intensity_df, randd_expenditure_df, manuf_val_added_df, urban_pop_pct_df)%>%
+  select(c(3, 5:(ncol(.) - 1)))%>%
+  pivot_longer(cols = 2:ncol(.), names_to = "year", values_to = "value")
+
+wb_df$year <- as.numeric(wb_df$year)
+
+wb_df <- pivot_wider(wb_df, names_from = `Indicator Name`, values_from = value)
+
+new_names_for_wb_df <- c("year", "trade_pct_of_gdp", "randd_exp_pct_of_gdp", "manuf_val_added_pct_of_gdp", "urban_pop_pct")
+
+names(wb_df) <- new_names_for_wb_df
+
+# need to add 2022: 2.69, and 2023: 2.64 to R&D expenditure (source: 2023 edition of https://www.ons.gov.uk/economy/governmentpublicsectorandtaxes/researchanddevelopmentexpenditure )
+wb_df$randd_exp_pct_of_gdp[wb_df$year == 2022] <- 2.69
+wb_df$randd_exp_pct_of_gdp[wb_df$year == 2023] <- 2.64
+
 # Life expectancy, Our World in Data
 # Data source: Riley (2005); Zijdeman et al. (2015); HMD (2025); UN WPP (2024) – Learn more about this data
 # OurWorldinData.org/life-expectancy | CC BY
 # https://ourworldindata.org/life-expectancy
-life_expectancy <- readr::read_csv("data/owid_life_expectancy_uk_61_23.csv")
-# need to add 2022: 2.69, and 2023: 2.64 (source: 2023 edition of https://www.ons.gov.uk/economy/governmentpublicsectorandtaxes/researchanddevelopmentexpenditure )
+life_expectancy <- readr::read_csv("data/owid_life_expectancy_uk_61_23.csv")%>%
+  select(year = Year,
+         life_expectancy = `Period life expectancy at birth`)
 
+
+
+# make a note of which variables are nominal, which need summarising by averaging or summing up to annual, which need dividing or multiplying by an index
+# Boe: all indices or rates so all fine
+# IFS: already inflation adjusted
+# ONS public sector finances: all are nominal!
+# GFCF is CVM (real) so all fine
+# housebuilding: all fine
+# capital account balance: current prices, need to adjust for inflation!
+# 
+
+# join all the data together
+joined_df <- list(df_x, df_y, df_z) %>% 
+  reduce(full_join, by = "i")
+
+#begin creating the time series graphs
